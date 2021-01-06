aff11565d090a0775c8a640ee871b2e9f7e32460
= <=>
2021-01-06 19:51:47 +0100

    Added currency button. Reworked after,before

--- js/buttonDown.js
+++ js/buttonDown.js
@@ -1,14 +1,57 @@
-var $but=$("main .bonus button");
-$but.click(function(e){
-    $(window).scrollTop(0);
-});
-$but.hide();
-$(window).scroll(function(e) {
-    var $height = $(window).scrollTop();
-    if($height > 10) {
-        $but.show();
+$(function ()  {
+    var i=0;
+    var $but=$('.but_home');
+    var $butDown=$("#arrowDown");
+    var $state=$("#listCurr span:first-Child");
+    var $state2;
+    var $span=$("#listCurr .euro span,#listCurr .usd span, #listCurr .btc span");
+    var $othCur= $("#listCurr .usd, #listCurr .btc");
+    styles = {
+        "background-color":"black",
+        "color":"white"
     }
-    else {
-        $but.hide();
+    styles1 = {
+        "margin-left":"7px"
     }
-});
+    //changing state currency block
+    $butDown.on("click",function (e) {
+        $othCur.toggle().css(styles);
+    });
+    $state.first().addClass("selected");
+    $state2=$('.selected').html();
+    $span.on("click",function () {
+        if (i==0) {
+            $('.selected').html($(this).html());
+            $(this).addClass('selected1').html($state2); 
+        }
+        else  {
+            $("#listCurr span").removeClass("selected1 selected");
+            $state.first().addClass("selected");
+            $state2=$('.selected').html();
+            $('.selected').html($(this).html());
+            $(this).addClass('selected1').html($state2); 
+        }
+        i++;
+    });
+    //:hoover
+    $othCur.on( "mouseover", function() {
+        $(this).css("color","#e0cba3");
+    }).on("mouseout",function () {
+        $(this).css("color","white");
+    })
+   
+    //movement button up
+    $but.hide();
+    $but.click(function(e){
+        $(window).scrollTop(0);
+    });
+    $(window).scroll(function(e) {
+        var $height = $(window).scrollTop();
+        if($height > 10) {
+            $but.show();
+        }
+        else {
+            $but.hide();
+        }
+    });
+})
--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
 $(document).ready(()=> {
+    //movement of accordion button 
     var button = $("#accordionMenu");
     var menuUpper=$(".upper-panel");
-    var menuUpperPhone = $("header .upper-panel .phone");
-    var menuUpperNav=$("header .upper-panel .menu");
-    var menuBottom=$("header .bottom-panel");
-    var buttCurr=$("header .upper-panel .currency");
-    //console.log(button,menu);
     button.on("click", function (){
         button.fadeOut("slow");
         menuUpper.fadeIn();
         //menuBottom.fadeIn();
-    })
-    buttCurr.on("click",fucntion() {
-        
     })
 })
